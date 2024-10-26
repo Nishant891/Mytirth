64bd81a27d59c7bc56d73ef0c49638c5315b197b
Nishant Sharma <nishant19072003@gmail.com>
2024-10-26 18:55:34 +0530

    image isuue resolved

--- app/lib/pilgrims.ts
+++ app/lib/pilgrims.ts
@@ -1,44 +1,45 @@
+
 export const pilgrims = [
     {
         id: 1,
         name: "Ayodhya",
         price: "₹59999",
-        url: "./ayodhya.jpeg",
+        url: "/ayodhya.jpeg",
         discount: "10%"
     },
     {
         id: 2,
         name: "Badrinath",
         price: "₹59999",
-        url: "./badrinath.jpeg",
+        url: "/badrinath.jpeg",
         discount: "10%"
     },
     {
         id: 3,
         name: "Maha Kumbh Mela",
         price: "₹59999",
-        url: "./kumbh.jpeg",
+        url: "/kumbh.jpeg",
         discount: "10%"
     },
     {
         id: 4,
         name: "Ujjain",
         price: "₹59999",
-        url: "./ujjain.jpeg",
+        url: "/ujjain.jpeg",
         discount: "10%"
     },
     {
         id: 5,
         name: "Vaishno Devi",
         price: "₹59999",
-        url: "./vaishno.jpeg",
+        url: "/vaishno.jpeg",
         discount: "10%"
     },
     {
         id: 6,
         name: "Ayodhya",
         price: "₹59999",
-        url: "./ayodhya.jpeg",
+        url: "/ayodhya.jpeg",
         discount: "10%"
     }
 ];
